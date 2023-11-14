16abf77be454c549a0f70a52b50476a8731aec50
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2023-11-14 13:29:39 -0600

    feat(images): add support for tapd v0.3.1-alpha

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -304,9 +304,10 @@ export const defaultRepoState: DockerRepoState = {
       versions: [],
     },
     tapd: {
-      latest: '0.3.0-alpha',
-      versions: ['0.3.0-alpha'],
+      latest: '0.3.1-alpha',
+      versions: ['0.3.1-alpha', '0.3.0-alpha'],
       compatibility: {
+        '0.3.1-alpha': '0.16.0-beta',
         '0.3.0-alpha': '0.16.0-beta',
       },
     },
